cdf6ad26b641c25a3578241ecfe0e9dbe080498e
Rocc <74003169+RoccIrch@users.noreply.github.com>
2024-02-21 00:10:59 +0100

    adding vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
-
+import { Analytics } from '@vercel/analytics/react';
 
 import "@/styles/globals.css";
 
 const App = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+    <>
+        <Component {...pageProps} />
+        <Analytics />
+    </>
 )
 
 export default App;
